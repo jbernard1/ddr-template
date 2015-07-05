dd06d34d88fcbad795576f4b17ddf766154d156a
Mark Steve Samson <hello@marksteve.com>
2015-07-05 09:14:42 +0800

    Integrate demo api with demo ui

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
+from api.viewsets import UserViewSet
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- frontend/static/jsx/main.jsx
+++ frontend/static/jsx/main.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import axios from 'axios'
 
-var Main = React.createClass({
+var Users = React.createClass({
+  getInitialState() {
+    return {
+      users: [],
+    }
+  },
+  componentWillMount() {
+    axios.get({
+      url: '/api/users/',
+      xsrfCookieName: 'csrftoken',
+    })
+    .then((response) => {
+      this.setState({
+        users: response.data,
+      })
+    })
+  },
+  renderUser(user) {
+    return (
+      <div className="user">
+        <strong>{user.username}</strong>
+        {user.email}
+      </div>
+    )
+  },
   render() {
-    return <div>Hello, world</div>
+    return (
+      <div className="users">
+        <h1>Users</h1>
+        {this.state.map(this.renderUser)}
+      </div>
+    )
   },
 })
 
-React.render(<Main />, document.body)
+React.render(<Users />, document.body)
